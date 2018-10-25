eadc5b65aba75eca75ae96ce3607b969a882bba1
Jostein Kjønigsen <jostein@kjonigsen.net>
2018-10-25 13:47:41 +0200

    Fix bad regexp references. Fix tests.

--- tests.py
+++ tests.py
@@ -5,21 +5,21 @@ import tsviz
 class Tests(unittest.TestCase):
     def test_parse_module_import_regexp(self):
         decl = "import { class } from \"./File\""
-        m = tsviz.module_declaration.match(decl)
+        m = tsviz.module_import_declaration.match(decl)
 
         self.assertNotEqual(None, m)
         [filename] = m.groups()
         self.assertEqual("File", filename)
 
         decl = "import { class, class 2 } from \"./File\""
-        m = tsviz.module_declaration.match(decl)
+        m = tsviz.module_import_declaration.match(decl)
 
         self.assertNotEqual(None, m)
         [filename] = m.groups()
         self.assertEqual("File", filename)
 
         decl = "import * as boo from \"./File\""
-        m = tsviz.module_declaration.match(decl)
+        m = tsviz.module_import_declaration.match(decl)
 
         self.assertNotEqual(None, m)
         [filename] = m.groups()
